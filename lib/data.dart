bfd9cc5accd85f7387b6ddb2b39b83233855f9d1
sarom chetra <chetrasarom@gmail.com>
2023-03-29 14:58:31 +0700

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,62 @@
+List foodCategory = [
+  {
+    "foodName": 'Salad',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2016/03/17/23/30/salad-1264107_1280.jpg'
+  },
+  {
+    "foodName": 'Pizza',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg'
+  },
+  {
+    "foodName": 'Cake',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2017/07/31/16/00/cake-2558610_1280.jpg'
+  },
+  {
+    "foodName": 'Burger',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2016/03/05/22/55/barbeque-1239407_1280.jpg'
+  },
+  {
+    "foodName": 'Drink',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2018/08/22/10/15/bottles-3623317_1280.jpg'
+  }
+];
+
+List foodTrendings = [
+  {
+    "foodName": 'Food1',
+    "review": "500 review's",
+    "calories": "100-300 calories",
+    "price": '\$12',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2014/05/05/19/52/charcuterie-338498__480.jpg'
+  },
+  {
+    "foodName": 'Food2',
+    "review": "500 review's",
+    "calories": "100-300 calories",
+    "price": '\$50',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_1280.jpg'
+  },
+  {
+    "foodName": 'Food3',
+    "review": "500 review's",
+    "calories": "100-300 calories",
+    "price": '\$22',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2022/06/07/20/52/curry-7249247_1280.jpg'
+  },
+  {
+    "foodName": 'Food4',
+    "review": "500 review's",
+    "calories": "100-300 calories",
+    "price": '\$30',
+    "imageUrl":
+        'https://cdn.pixabay.com/photo/2020/06/28/14/14/pizza-5349437_1280.jpg'
+  }
+];
